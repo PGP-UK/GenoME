67bf952053d2f285ad4ffc498b911882d6a6a936
Ismail Moghul <ismail.moghul@gmail.com>
2021-09-19 22:15:07 +0100

    Move AppNavigator to a separate file

--- App.js
+++ App.js
@@ -1,70 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
-
-import Home from './screens/Home';
-import About from './screens/About';
-import Age from './screens/individual/Age';
-import Ancestry from './screens/individual/Ancestry';
-import Eyes from './screens/individual/Eyes';
-import Health from './screens/individual/Health';
-import Landing from './screens/individual/Landing';
-import Smoking from './screens/individual/Smoking';
-import Variant from './screens/individual/Variant';
-
-const Stack = createStackNavigator();
+import AppNavigator from './components/AppNavigator';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name='Home' component={Home} />
-          <Stack.Screen name='About' component={About} />
-
-          <Stack.Screen
-            name='Variant'
-            component={Variant}
-            initialParams={{ name: 'Stephan' }}
-          />
-          <Stack.Screen
-            name='Landing'
-            component={Landing}
-            initialParams={{ name: 'Stephan' }}
-          />
-
-          <Stack.Screen
-            name='Ancestry'
-            component={Ancestry}
-            initialParams={{ name: 'Stephan' }}
-          />
-          <Stack.Screen
-            name='Age'
-            component={Age}
-            initialParams={{ name: 'Stephan' }}
-          />
-          <Stack.Screen
-            name='Eyes'
-            component={Eyes}
-            initialParams={{ name: 'Stephan' }}
-          />
-          <Stack.Screen
-            name='Health'
-            component={Health}
-            initialParams={{ name: 'Stephan' }}
-          />
-          <Stack.Screen
-            name='Smoking'
-            component={Smoking}
-            initialParams={{ name: 'Stephan' }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <AppNavigator />
       <StatusBar style='auto' />
     </SafeAreaProvider>
   );

65adcdeb9ad64c9571dffa22bc9e92951474be28
ishaqmoghul <ishaqa98@gmail.com>
2021-09-19 17:28:49 +0100

    added home page

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import Home from './screens/Home'
+
+const App = () => {
   return (
     <View style={styles.container}>
+      <Home />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -19,3 +22,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;

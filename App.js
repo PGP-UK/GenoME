e3e519b2d528757ad6efee1678c7a69197bc749a
Ismail Moghul <ismail.moghul@gmail.com>
2021-09-19 22:27:22 +0100

    Add Eva set up

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 import AppNavigator from './components/AppNavigator';
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <AppNavigator />
-      <StatusBar style='auto' />
-    </SafeAreaProvider>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.dark}>
+        <AppNavigator />
+        <StatusBar style='auto' />
+      </ApplicationProvider>
+    </>
   );
 };
 

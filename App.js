32ae346e7ae1bcd1cfc8c8c8b46637404f1b6a61
Ismail Moghul <ismail.moghul@gmail.com>
2021-09-26 15:45:00 +0100

    fix missing import
    Co-authored-by: E-gonito <E-gonito@users.noreply.github.com>
    Co-authored-by: Nathaniel Donato <NateCreedDonato@users.noreply.github.com>
    Co-authored-by: Ishaq Moghul <ishaqmoghul@users.noreply.github.com>
    Co-authored-by: Ron Finn <ronfinn@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';

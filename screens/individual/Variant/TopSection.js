94cab27d65226b57ac979796cd0cfb372cbde1cc
IsmailM <ismail.moghul@gmail.com>
2021-10-03 17:37:44 +0100

    add variant page

---
+++ screens/individual/Variant/TopSection.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Text } from '@ui-kitten/components';
+import { Section, Block } from 'react-native-responsive-layout';
+
+const TextSection = ({ textStyle, lines }) => (
+  <>
+    {lines.map((l, i) => (
+      <Text key={i} style={textStyle}>
+        {l}
+      </Text>
+    ))}
+  </>
+);
+
+const LeftText = ({ textStyle, data }) => (
+  <TextSection
+    textStyle={textStyle}
+    lines={['Total Number of variants:', data.num, data.percent]}
+  />
+);
+
+const RightText = ({ textStyle, data }) => (
+  <TextSection
+    textStyle={textStyle}
+    lines={['Number of shared variants:', data.num, data.percent]}
+  />
+);
+
+const TopSection = ({ data }) => (
+  <Section style={styles.main}>
+    {/* For small screens */}
+    <Block xsSize='100%' mdHidden style={styles.textRowSm}>
+      <LeftText data={data.total} textStyle={styles.leftTextSm} />
+    </Block>
+    <Block xsSize='100%' mdHidden style={styles.textRowSm}>
+      <RightText
+        data={data.shared}
+        textStyle={{ ...styles.rightTextSm, color: data.themeColor }}
+      />
+    </Block>
+
+    {/* For larger screens */}
+    <Block xsSize='50%' hidden mdVisible>
+      <LeftText data={data.total} textStyle={styles.leftTextMd} />
+    </Block>
+    <Block xsSize='50%' hidden mdVisible>
+      <RightText
+        data={data.shared}
+        textStyle={{ ...styles.rightTextMd, color: data.themeColor }}
+      />
+    </Block>
+  </Section>
+);
+
+const styles = StyleSheet.create({
+  main: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  textRowSm: {
+    marginTop: 20,
+  },
+  leftTextSm: {
+    fontSize: 35,
+    color: '#45B0D4',
+    textAlign: 'center',
+  },
+  leftTextMd: {
+    fontSize: 35,
+    color: '#45B0D4',
+    textAlign: 'left',
+  },
+  rightTextSm: {
+    fontSize: 35,
+    textAlign: 'center',
+  },
+  rightTextMd: {
+    fontSize: 35,
+    textAlign: 'right',
+  },
+});
+
+export default TopSection;
---
+++ screens/individual/Variant/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { StyleSheet, Image, View, Dimensions } from 'react-native';
+import PageLayout from '../../../components/PageLayout';
+import { Grid, Section, Block } from 'react-native-responsive-layout';
+import TopSection from './TopSection';
+import BottomSection from './BottomSection';
+
+const AllAmbassadorsData = {
+  stephan: {
+    data: require(`../../../assets/variants/stephan/data.json`),
+    image: require(`../../../assets/variants/stephan/pie_chart.png`),
+  },
+  colin: {
+    data: require(`../../../assets/variants/colin/data.json`),
+    image: require(`../../../assets/variants/colin/pie_chart.png`),
+  },
+  laura: {
+    data: require(`../../../assets/variants/laura/data.json`),
+    image: require(`../../../assets/variants/laura/pie_chart.png`),
+  },
+  momodou: {
+    data: require(`../../../assets/variants/momodou/data.json`),
+    image: require(`../../../assets/variants/momodou/pie_chart.png`),
+  },
+};
+
+const Variant = (props) => {
+  const { route } = props;
+  const { name } = route.params;
+
+  return (
+    <>
+      <PageLayout>
+        <TopSection data={AllAmbassadorsData[name].data} />
+        <Section stretch>
+          <Block size='stretch'>
+            <View
+              style={{
+                justifyContent: 'center',
+                flex: 1,
+              }}>
+              <Image
+                source={AllAmbassadorsData[name].image}
+                style={[styles.image]}
+              />
+            </View>
+          </Block>
+        </Section>
+        <BottomSection data={AllAmbassadorsData[name].data} />
+      </PageLayout>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  image: {
+    flex: 1,
+    alignSelf: 'center',
+    width: 600,
+    height: 600,
+    maxWidth: '80%',
+    resizeMode: 'contain',
+  },
+});
+
+export default Variant;

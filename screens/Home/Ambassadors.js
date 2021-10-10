29ac727bd674f7869db1d32c15e5057738d846a6
ishaqmoghul <ishaqa98@gmail.com>
2021-10-10 22:14:10 +0100

    deleted icons from main folder and moved to images folder and edited landing page

--- screens/Home/Ambassadors.js
+++ screens/Home/Ambassadors.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import { StyleSheet, Image, Pressable } from 'react-native';
-import { Text } from '@ui-kitten/components';
-import { Grid, Section, Block } from 'react-native-responsive-layout';
-import { withSizeInfo } from 'react-native-responsive-layout/wrappers';
-import { useSafeAreaFrame } from 'react-native-safe-area-context';
+import React from "react";
+import { StyleSheet, Image, Pressable } from "react-native";
+import { Text } from "@ui-kitten/components";
+import { Grid, Section, Block } from "react-native-responsive-layout";
+import { withSizeInfo } from "react-native-responsive-layout/wrappers";
+import { useSafeAreaFrame } from "react-native-safe-area-context";
 
 const AllAmbassadors = [
   {
-    name: 'Stephan',
-    hexId: 'uk35C650',
-    image: require('../../assets/images/home_screen_profiles/stephan.gif'),
-    themeColor: '#8CD8C4',
+    name: "Stephan",
+    hexId: "uk35C650",
+    image: require("../../assets/images/home_screen_profiles/stephan.gif"),
+    themeColor: "#8CD8C4",
   },
   {
-    name: 'Laura',
-    hexId: 'uk33D02F',
-    image: require('../../assets/images/home_screen_profiles/laura.gif'),
-    themeColor: '#F6BD4A',
+    name: "Laura",
+    hexId: "uk33D02F",
+    image: require("../../assets/images/home_screen_profiles/laura.gif"),
+    themeColor: "#F6BD4A",
   },
   {
-    name: 'Momodou',
-    hexId: 'uk481F67',
-    image: require('../../assets/images/home_screen_profiles/momodou.gif'),
-    themeColor: '#D94553',
+    name: "Momodou",
+    hexId: "uk481F67",
+    image: require("../../assets/images/home_screen_profiles/momodou.gif"),
+    themeColor: "#D94553",
   },
   {
-    name: 'Colin',
-    hexId: 'uk4CA868',
-    image: require('../../assets/images/home_screen_profiles/colin.gif'),
-    themeColor: '#9C82DE',
+    name: "Colin",
+    hexId: "uk4CA868",
+    image: require("../../assets/images/home_screen_profiles/colin.gif"),
+    themeColor: "#9C82DE",
   },
 ];
 
@@ -51,9 +51,10 @@ const Ambassador = (props) => {
     <Block xsSize="100%" smSize="50%" mdSize="25%">
       <Pressable
         onPress={() =>
-          navigation.navigate('Ancestry', { name: name.toLowerCase() })
+          navigation.navigate("Landing", { name: name.toLowerCase() })
         }
-        style={styles.box}>
+        style={styles.box}
+      >
         <AmbassadorImage source={image} />
         <Text category="p1" style={[styles.header, { color: themeColor }]}>
           {hexId}
@@ -78,19 +79,19 @@ const Ambassadors = ({ navigation }) => (
 
 const styles = StyleSheet.create({
   box: {
-    alignItems: 'center',
+    alignItems: "center",
     padding: 10,
     marginTop: 10,
     marginBottom: 10,
   },
   header: {
     fontSize: 24,
-    fontWeight: '500',
+    fontWeight: "500",
     marginTop: 10,
   },
   secondLineText: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
 
--- screens/individual/Landing.js
+++ screens/individual/Landing.js
@@ -1,22 +1,63 @@
-import React from 'react';
-import { StyleSheet, Image, View } from 'react-native';
-import { Text } from '@ui-kitten/components';
-import PageLayout from '../../components/PageLayout';
-import { Section, Block, Grid } from 'react-native-responsive-layout';
+import React from "react";
+import { StyleSheet, Image, View, Pressable } from "react-native";
+import { Text } from "@ui-kitten/components";
+import PageLayout from "../../components/PageLayout";
+import { Section, Block, Grid } from "react-native-responsive-layout";
+import { withSizeInfo } from "react-native-responsive-layout/wrappers";
+import { useSafeAreaFrame } from "react-native-safe-area-context";
 
+const Icons = [
+  {
+    pageName: "Ancestry",
+    image: require("../../assets/images/landing_screen_icons/globe-europe-duotone.png"),
+  },
+  {
+    pageName: "Eyes",
+    image: require("../../assets/images/landing_screen_icons/eye-duotone.png"),
+  },
+  {
+    pageName: "Health",
+    image: require("../../assets/images/landing_screen_icons/heartbeat-duotone.png"),
+  },
+  {
+    pageName: "Age",
+    image: require("../../assets/images/landing_screen_icons/birthday-cake-duotone.png"),
+  },
+  {
+    pageName: "Smoking",
+    image: require("../../assets/images/landing_screen_icons/smoking-duotone.png"),
+  },
+];
 
+const Iconimage = withSizeInfo(({ sizeSelector, ...props }) => {
+  const numImagesPerRow = sizeSelector({ xs: 1, sm: 2, md: 5 });
+  const imageWidth =
+    Math.round(useSafeAreaFrame().width / numImagesPerRow) - 40;
+  return (
+    <Image
+      {...props}
+      style={{ width: imageWidth, height: imageWidth }}
+      resizeMode="contain"
+    />
+  );
+});
 
 const Landing = (props) => {
-  const { route } = props;
+  const { route, navigation } = props;
   const { name } = route.params;
   return (
     <>
-      <PageLayout >
+      <PageLayout>
         <Text category="h1" style={styles.header}>
-          My GENOME contains many layers of INFORMATION, including GENETIC and EPIGENETIC information.
+          My GENOME contains many layers of INFORMATION, including GENETIC and
+          EPIGENETIC information.
         </Text>
         <Text category="h5" style={styles.subheader}>
-          My DNA sequence is about 99.9% identical to other people's DNA. the remaining 0.1% is what makes me unique, which amounts to several million changes. These changes are called "genetic variants". The majority of variants are shared between individuals whilst others are private (in this case, unique to me or my family).  
+          My DNA sequence is about 99.9% identical to other people's DNA. the
+          remaining 0.1% is what makes me unique, which amounts to several
+          million changes. These changes are called "genetic variants". The
+          majority of variants are shared between individuals whilst others are
+          private (in this case, unique to me or my family).
         </Text>
         <Text category="h5" style={styles.main_text}>
           What is a PRIVATE, GENETIC, or EPIGENETIC variant?
@@ -24,53 +65,38 @@ const Landing = (props) => {
         <Text category="h5" style={styles.main_text}>
           Tap below to explore a few of my variants which we do know about.
         </Text>
+
         <Grid>
-          <Section>
-            <Block xsSize="100%" mdSize="20%">
-              <View style={styles.avcontainer}>
-                <Image
-                  style={styles.avpic}
-                  source={require('./globe-europe-duotone.png')}
-                />
-                <Text style={{paddingTop: 10, fontSize: 25}}>Ancestry</Text>
-              </View>
-            </Block >
-            <Block xsSize="100%" mdSize="20%">
-              <View style={styles.avcontainer}>
-                <Image
-                  style={styles.avpic}
-                  source={require('./eye-duotone.png')}
-                />
-                <Text style={{paddingTop: 10, fontSize: 25}}>Eyes</Text>
-              </View>
-            </Block>
-            <Block xsSize="100%" mdSize="20%">
-              <View style={styles.avcontainer}>
-                <Image
-                  style={styles.avpic}
-                  source={require('./heartbeat-duotone.png')}
-                />
-                <Text style={{paddingTop: 10, fontSize: 25}}>Health</Text>
-              </View>
-            </Block>
-            <Block xsSize="100%" mdSize="20%">
-              <View style={styles.avcontainer}>
-                <Image
-                  style={styles.avpic}
-                  source={require('./birthday-cake-duotone.png')}
-                />
-                <Text style={{paddingTop: 10, fontSize: 25}}>Age</Text>
-              </View>
-            </Block>
-            <Block xsSize="100%" mdSize="20%">
-              <View style={styles.avcontainer}>
-                <Image
-                  style={styles.avpic}
-                  source={require('./smoking-duotone.png')}
-                />
-                <Text style={{paddingTop: 10, fontSize: 25}}>Smoking</Text>
-              </View>
-            </Block>
+          <Section style={{ marginTop: 30 }}>
+            {Icons.map((IconData) => (
+              <Block
+                xsSize="100%"
+                smSize="50%"
+                mdSize="20%"
+                style={{ alignItems: "center" }}
+              >
+                <Pressable
+                  onPress={() =>
+                    navigation.navigate(IconData.pageName, {
+                      name: name.toLowerCase(),
+                    })
+                  }
+                  style={styles.box}
+                >
+                  <Iconimage source={IconData.image} />
+                  <Text
+                    category="p1"
+                    style={{
+                      paddingTop: 20,
+                      fontSize: 25,
+                      textAlign: "center",
+                    }}
+                  >
+                    {IconData.pageName}
+                  </Text>
+                </Pressable>
+              </Block>
+            ))}
           </Section>
         </Grid>
       </PageLayout>
@@ -84,8 +110,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     marginLeft: 30,
     fontSize: 85,
-    textAlign: 'center'
-    
+    textAlign: "center",
   },
   subheader: {
     marginTop: 10,
@@ -93,7 +118,7 @@ const styles = StyleSheet.create({
     paddingLeft: 70,
     paddingRight: 70,
     fontSize: 30,
-    textAlign: 'center'
+    textAlign: "center",
   },
   main_text: {
     marginTop: 10,
@@ -101,7 +126,7 @@ const styles = StyleSheet.create({
     paddingLeft: 100,
     paddingRight: 100,
     fontSize: 20,
-    textAlign: 'center'
+    textAlign: "center",
   },
   avpic: {
     width: 200,
@@ -111,10 +136,10 @@ const styles = StyleSheet.create({
     borderWidth: 3,
   },
   avcontainer: {
-    alignItems: 'center',
-    flexWrap: 'wrap',
+    alignItems: "center",
+    flexWrap: "wrap",
     padding: 8,
-    flexWrap: 'wrap', 
+    flexWrap: "wrap",
   },
 });
 

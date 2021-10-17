b286aaeaa611982ba8e3a13484f04edd10e79656
E-gonito <masterdank47@gmail.com>
2021-10-17 01:48:54 +0100

    added all components to the heart disease screen

--- screens/individual/Health/Heart/index.js
+++ screens/individual/Health/Heart/index.js
@@ -1,25 +1,88 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Layout, Text, Button } from '@ui-kitten/components';
+import { StyleSheet, Image, View } from 'react-native';
+import { Text, Card } from '@ui-kitten/components';
 import { Grid, Section, Block } from 'react-native-responsive-layout';
 import PageLayout from '../../../../components/PageLayout';
 
+const AllAmbassadorsData = {
+  stephan: {
+    data: require(`../../../../assets/data/health/heart/stephan.json`),
+    image: require(`../../../../assets/images/heart_profiles/stephan_heart_profile.png`),
+  },
+  colin: {
+    data: require(`../../../../assets/data/health/heart/colin.json`),
+    image: require(`../../../../assets/images/heart_profiles/colin_heart_profile.png`),
+  },
+  laura: {
+    data: require(`../../../../assets/data/health/heart/laura.json`),
+    image: require(`../../../../assets/images/heart_profiles/laura_heart_profile.png`),
+  },
+  momodou: {
+    data: require(`../../../../assets/data/health/heart/momodou.json`),
+    image: require(`../../../../assets/images/heart_profiles/momodou_heart_profile.png`),
+  },
+};
+
 const Heart = (props) => {
-  const { route, navigation } = props;
+  const { route } = props;
   const { name } = route.params;
+  const data = AllAmbassadorsData[name].data;
+
   return (
     <>
       <PageLayout>
         <Grid>
           <Section>
-            <Block>
+            <Block xsSize="100%" smSize="100%" mdSize="50%">
               <Text category="h1" style={styles.header}>
                 Heart disease
               </Text>
             </Block>
-            <Block>
+            <Block xsSize="100%" smSize="100%" mdSize="50%">
+              <Text
+                category="h1"
+                style={(styles.header, { color: data.themeColor })}>
+                Genetic variant ID: {'\n'}rs238206
+              </Text>
+            </Block>
+          </Section>
+          <Section>
+            <Block xsSize="100%" smSize="100%" mdSize="33%">
+              <Text category="h1" style={styles.header2}>
+                Risk with each variant type:
+              </Text>
+              <Text category="h1" style={styles.header3}>
+                <View style={styles.square1} />
+                {data.risk.first}
+              </Text>
+              <Text category="h1" style={styles.header3}>
+                <View style={styles.square2} />
+                {data.risk.second}
+              </Text>
+              <Text category="h1" style={styles.header3}>
+                <View style={styles.square3} />
+                {data.risk.third}
+              </Text>
+            </Block>
+            <Block xsSize="100%" smSize="100%" mdSize="33%">
+              <Image
+                source={AllAmbassadorsData[name].image}
+                style={styles.image}
+                resizeMode="contain"
+              />
+            </Block>
+            <Block xsSize="100%" smSize="100%" mdSize="33%">
+              <Card>
+                <Text style={styles.main_text}>
+                  There are three versions of this variant and{' '}
+                  {data.population.percent} of the population have the same
+                  variant as me {'\n'}This version of the variant increases my
+                  risk of heart disease
+                </Text>
+              </Card>
               <Text category="p1" style={styles.main_text}>
-                This is {name} Page
+                The colour split illustrates how common each variant type is in
+                the population.
               </Text>
             </Block>
           </Section>
@@ -53,6 +116,16 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 25,
   },
+  header2: {
+    color: '#666E7A',
+    fontWeight: '400',
+    marginTop: 20,
+    marginBottom: 25,
+  },
+  header3: {
+    color: '#666E7A',
+    fontWeight: '400',
+  },
   main_text: {
     color: '#666E7A',
     fontSize: 20,
@@ -62,6 +135,25 @@ const styles = StyleSheet.create({
     color: '#666E7A',
     fontSize: 20,
     marginBottom: 20,
+  },
+  image: {
+    height: 250,
+    width: 250,
+  },
+  square1: {
+    width: 25,
+    height: 25,
+    backgroundColor: 'red',
+  },
+  square2: {
+    width: 25,
+    height: 25,
+    backgroundColor: 'red',
+  },
+  square3: {
+    width: 25,
+    height: 25,
+    backgroundColor: 'red',
   },
 });
 

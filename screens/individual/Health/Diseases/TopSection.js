68804ff836f67f3356a0c497325d260442ffb13a
E-gonito <masterdank47@gmail.com>
2021-10-31 21:01:03 +0000

    Refactored TopSection.js by removing duplicate code

--- screens/individual/Health/Diseases/TopSection.js
+++ screens/individual/Health/Diseases/TopSection.js
@@ -3,62 +3,39 @@ import { Text } from '@ui-kitten/components';
 import { Section, Block } from 'react-native-responsive-layout';
 import { StyleSheet } from 'react-native';
 
+const textInformation = {
+  heart: {
+    heading: 'Heart Disease',
+    id: 'rs238206',
+  },
+  crohn: {
+    heading: "Crohn's disease",
+    id: 'rs2066847',
+  },
+  ovarian: {
+    heading: 'Ovarian cancer',
+    id: 'rs3218536',
+  },
+};
+
 const TopSection = (props) => {
   const { disease, data } = props;
-  if (disease === 'heart') {
-    return (
-      <Section style={{ paddingBottom: 50 }}>
-        <Block xsSize="100%" smSize="100%" mdSize="70%">
-          <Text category="h1" style={styles.header}>
-            Heart Disease
-          </Text>
-        </Block>
-        <Block xsSize="100%" smSize="100%" mdSize="30%">
-          <Text
-            category="h1"
-            style={[styles.header, { color: data.themeColor }]}>
-            Genetic variant ID: {'\n'}rs238206
-          </Text>
-        </Block>
-      </Section>
-    );
-  }
-  if (disease === 'crohn') {
-    return (
-      <Section style={{ paddingBottom: 50 }}>
-        <Block xsSize="100%" smSize="100%" mdSize="70%">
-          <Text category="h1" style={styles.header}>
-            Crohn's disease
-          </Text>
-        </Block>
-        <Block xsSize="100%" smSize="100%" mdSize="30%">
-          <Text
-            category="h1"
-            style={[styles.header, { color: data.themeColor }]}>
-            Genetic variant ID: {'\n'}rs2066847
-          </Text>
-        </Block>
-      </Section>
-    );
-  }
-  if (disease === 'ovarian') {
-    return (
-      <Section style={{ paddingBottom: 50 }}>
-        <Block xsSize="100%" smSize="100%" mdSize="70%">
-          <Text category="h1" style={styles.header}>
-            Ovarian disease
-          </Text>
-        </Block>
-        <Block xsSize="100%" smSize="100%" mdSize="30%">
-          <Text
-            category="h1"
-            style={[styles.header, { color: data.themeColor }]}>
-            Genetic variant ID: {'\n'}rs3218536
-          </Text>
-        </Block>
-      </Section>
-    );
-  }
+  const textData = textInformation[disease];
+  return (
+    <Section style={{ paddingBottom: 50 }}>
+      <Block xsSize="100%" smSize="100%" mdSize="70%">
+        <Text category="h1" style={styles.header}>
+          {textData.heading}
+        </Text>
+      </Block>
+      <Block xsSize="100%" smSize="100%" mdSize="30%">
+        <Text category="h1" style={[styles.header, { color: data.themeColor }]}>
+          Genetic variant ID: {'\n'}
+          {textData.id}
+        </Text>
+      </Block>
+    </Section>
+  );
 };
 
 const styles = StyleSheet.create({

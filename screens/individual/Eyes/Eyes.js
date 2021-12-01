de1976253fa58aeb5c23342184d08f41cb69c13d
Ron Finn <ronfinn@users.noreply.github.com>
2021-12-01 21:33:30 +0000

    Basic framework for Eyes.js added, music and videos added to assets
    Moved Eyes.js to Eyes folder. Much more work needed for split screen and eye_colour.png on other half of split screen.
    GenoME.mp3 added to assets/music.
    Ambassador eye colour mp4s added to assets/videos.

---
+++ screens/individual/Eyes/Eyes.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Text } from '@ui-kitten/components';
+import PageLayout from '../../components/PageLayout';
+
+const Eyes = () => {
+  return (
+    <>
+      <PageLayout>
+      <View style={styles.mainContainer}>
+        <View style={styles.navBar} />
+
+        <Text category="h1" style={styles.header}>
+          Eye colour
+        </Text>
+
+        <View style={styles.body} />
+
+        <Text category="p1" style={styles.main_text}>
+          Eye colour is determined by multiple genetic variants, some of which have been 
+          used to predict my eye colour. Below you can see my actual eyes and the 
+          predicted eye colour - how well do you think the prediction has 
+          worked?
+        </Text>
+        </View>
+      </PageLayout>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  navBar: {
+    display: 'flex',
+    flexDirection: 'row-reverse',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#f1f1f1',
+    height: 30,
+  },
+  body: {
+    flex: 3,
+    display: 'flex',
+  },
+  header: {
+    fontWeight: '400',
+    marginTop: 20,
+    marginBottom: 25,
+  },
+  main_text: {
+    color: '#666E7A',
+    fontSize: 20,
+    marginBottom: 20,
+  },
+});
+
+export default Eyes;

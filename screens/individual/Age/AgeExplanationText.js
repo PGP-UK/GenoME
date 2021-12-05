84863549e7dc2bcce7e9f505e1995fa25e82cf61
Nathaniel Donato <nathanieldonatofx@gmail.com>
2021-12-05 17:30:08 +0200

    created a new page to split up the smoking page into workable parts
    This is not the final version of the pages that I am working on, I will push and create another pull request when I have completed the rest of the formatting work

--- screens/individual/Age/AgeExplanationText.js
+++ screens/individual/Age/AgeExplanationText.js
@@ -7,23 +7,22 @@ const AgeExplanationText = ({ themeColor }) => {
     <Block xsSize="100%" mdHidden>
       <Text style={[styles.paragraph, { backgroundColor: themeColor }]}>
         Epigenetic age was calculated from 353 epigenetic variants across the
-        participant's genome. These epigenetic variants correlate very well
-        with actual age, meaning they can be used as an 'epigenetic clock' to
-        predict age. Age acceleration applies if the epigenetic age is 3.6
-        years older than the actual age. Age deceleration applies if the
-        epigenetic age is 3.6 years younger than their actual age.
+        participant's genome. These epigenetic variants correlate very well with
+        actual age, meaning they can be used as an 'epigenetic clock' to predict
+        age. Age acceleration applies if the epigenetic age is 3.6 years older
+        than the actual age. Age deceleration applies if the epigenetic age is
+        3.6 years younger than their actual age.
       </Text>
     </Block>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
   paragraph: {
     fontSize: 20,
     color: 'white',
     fontWeight: '400',
-    padding: 10
-  }
+    padding: 10,
+  },
 });
 
-
-export default AgeExplanationText
+export default AgeExplanationText;

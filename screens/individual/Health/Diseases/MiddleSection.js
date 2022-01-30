82602d54c2d69ff2befe44ff8e3035f64496dbd5
E-gonito <65162745+E-gonito@users.noreply.github.com>
2022-01-30 13:32:20 +0000

    #67 changed tablet layout of individual health screens

--- screens/individual/Health/Diseases/MiddleSection.js
+++ screens/individual/Health/Diseases/MiddleSection.js
@@ -8,7 +8,7 @@ import { PageHeader } from '../../../../components/Text';
 
 const AmbassadorImage = withSizeInfo(({ sizeSelector, ...props }) => {
   const ImageStyles = sizeSelector({ xs: styles.imageSm, md: styles.imageMd });
-  const ImagePercent = sizeSelector({ xs: 0.8, md: 0.3 });
+  const ImagePercent = sizeSelector({ xs: 0.8, md: 0.3, lg: 0.3, xl: 0.4 });
   const imageWidth = Math.round(useSafeAreaFrame().width * ImagePercent) - 40;
   const maxImageHeight = Math.round(useSafeAreaFrame().height * 0.8);
   const finalImageWidth =
@@ -44,24 +44,61 @@ const VariantRisk = withSizeInfo(({ sizeSelector, riskData }) => {
 
   return (
     <View>
-      <PageText style={styles.header2}>Risk with each variant type:</PageText>
-      <PageHeader category="h4" style={ImageStyles}>
-        <View style={styles.square1} />
-        &nbsp;
-        {riskData.first}
-      </PageHeader>
-      <PageHeader category="h4" style={ImageStyles}>
-        <View style={styles.square2} />
-        &nbsp;
-        {riskData.second}
-      </PageHeader>
-      <PageHeader category="h4" style={ImageStyles}>
-        <View style={styles.square3} />
-        &nbsp;
-        {riskData.third}
-      </PageHeader>
+      <Block
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignContent: 'center',
+        }}>
+        <PageText style={styles.header2}>Risk with each variant type:</PageText>
+      </Block>
+      {/* Hidden in large screens */}
+      <Block xlHidden>
+        <PageHeader category="h4" style={ImageStyles}>
+          <View style={styles.square1} />
+          &nbsp;
+          {riskData.first}
+        </PageHeader>
+        <PageHeader category="h4" style={ImageStyles}>
+          <View style={styles.square2} />
+          &nbsp;
+          {riskData.second}
+        </PageHeader>
+        <PageHeader category="h4" style={ImageStyles}>
+          <View style={styles.square3} />
+          &nbsp;
+          {riskData.third}
+        </PageHeader>
+      </Block>
+
       {/* Hidden in small screens */}
-      <Block hidden mdVisible>
+      <Block
+        hidden
+        xlVisible
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignContent: 'center',
+        }}>
+        <PageHeader category="h4" style={ImageStyles}>
+          <View style={styles.square1} />
+          &nbsp;
+          {riskData.first}
+          &nbsp;
+          <View style={styles.square2} />
+          &nbsp;
+          {riskData.second}
+          &nbsp;
+          <View style={styles.square3} />
+          &nbsp;
+          {riskData.third}
+          &nbsp;
+        </PageHeader>
+      </Block>
+      {/* Hidden in small screens */}
+      <Block hidden mdVisible xlHidden>
         <ColorSplitText />
       </Block>
     </View>
@@ -84,25 +121,60 @@ const MiddleSection = (props) => {
           flexWrap: 'wrap',
         }}>
         {/* Hidden in large screens */}
-        <Block mdHidden>
+        <Block
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            alignContent: 'center',
+            paddingBottom: 20,
+          }}
+          mdHidden
+          xlVisible>
           <ColorSplitText />
         </Block>
 
-        {/* Hidden in small screens */}
-        <Block xsSize="100%" mdSize="33%" hidden mdVisible>
+        {/* Hidden in large screens */}
+        <Block
+          xsSize="100%"
+          smSize="100%"
+          mdSize="33%"
+          lgSize="33%"
+          xlSize="50%"
+          hidden
+          mdVisible
+          xlHidden>
           <VariantRisk riskData={data.risk} />
         </Block>
 
-        <Block xsSize="100%" mdSize="33%" style={{ paddingBottom: 20 }}>
+        <Block
+          xsSize="100%"
+          smSize="100%"
+          mdSize="33%"
+          lgSize="33%"
+          xlSize="50%"
+          style={{ paddingBottom: 20 }}>
           <AmbassadorImage source={image} />
         </Block>
 
         {/* Hidden in small screens */}
-        <Block xsSize="100%" mdSize="33%" mdHidden>
+        <Block
+          xsSize="100%"
+          smSize="100%"
+          mdSize="33%"
+          lgSize="33%"
+          xlSize="50%"
+          mdHidden>
           <VariantRisk riskData={data.risk} />
         </Block>
 
-        <Block xsSize="100%" mdSize="33%" style={{ paddingTop: 30 }}>
+        <Block
+          xsSize="100%"
+          smSize="100%"
+          mdSize="33%"
+          lgSize="33%"
+          xlSize="40%"
+          style={{ paddingTop: 30 }}>
           <PageText
             style={[styles.box_text, { backgroundColor: backgroundColour }]}>
             There are three versions of this variant and{' '}
@@ -111,6 +183,17 @@ const MiddleSection = (props) => {
             {'\n'}
             {data.message}
           </PageText>
+        </Block>
+
+        <Block
+          xsSize="100%"
+          smSize="100%"
+          mdSize="33%"
+          lgSize="33%"
+          xlSize="100%"
+          hidden
+          xlVisible>
+          <VariantRisk riskData={data.risk} />
         </Block>
       </Section>
     </>

c8f14bc819fa39bfd43e5258e4b71259f5f5657e
Ron Finn <ronfinn@users.noreply.github.com>
2022-02-07 21:12:20 +0000

    Alterations to About Us
    Progress on adding linking to page and slight formatting changes

--- screens/About.js
+++ screens/About.js
@@ -1,45 +1,65 @@
 import React from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, Linking } from 'react-native';
 import PageLayout from '../components/PageLayout';
-import HeaderRow from './individual/About/HeaderRow';
 import SocialMedia from './individual/About/SocialMedia';
-import { PageText } from '../components/Text';
-import { Section } from 'react-native-responsive-layout';
+import { PageHeader, PageText } from '../components/Text';
+import { Section, Block } from 'react-native-responsive-layout';
 
 const About = (props) => {
   const { navigation } = props;
   return (
     <>
       <PageLayout>
-       <HeaderRow navigation={navigation} HeaderStyle={styles.headerRow} />
-        <PageText style={styles.main_text}>
-          The PGP-UK project is part of the global network of Personal Genome Projects, which all share
-          the vision of advancing science through open data sharing. Currently, there are projects in America,
-          Austria, Canada, China and the UK. There are multiple ways to join the PGP-UK community and keep-up-date 
-          with the project - just follow the links.
+      <PageHeader style={styles.header}>
+        About PGP-UK and the global network of Personal Genome Projects
+      </PageHeader>
+      <Section
+          stretch
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            alignContent: 'center',
+            flexWrap: 'wrap',
+          }}>
+      <Block>
+      <PageText style={styles.main_text}>
+        The PGP-UK project is part of the global network of Personal Genome Projects, which all share
+        the vision of advancing science through open data sharing. Currently, there are projects in America,
+        Austria, Canada, China and the UK. There are multiple ways to join the PGP-UK community and keep-up-date 
+        with the project - just follow the links.
         </PageText>
-
+        </Block>
+        </Section>
         <Section
           stretch
           style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
             alignItems: 'center',
             alignContent: 'center',
-            paddingBottom: 50,
+            flexWrap: 'wrap',
           }}>
+        <Block>
          <SocialMedia navigation={navigation} SocialMediaStyle={styles.socialMedia}/>
+        </Block>
         </Section>
        
         <Section
           stretch
           style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
             alignItems: 'center',
             alignContent: 'center',
-            paddingBottom: 50,
+            flexWrap: 'wrap',
           }}>
+        <Block>
          <ImageBackground style={styles.bottomImage} source={require("../assets/images/about_us/more-information_globe.png")} >
-       
-        
-          
+         
           <PageText style={styles.bottom_red_text}>
             DISCLAIMER: The generation of genomic data is not an error-free process and our analysis system is 
             still under development. becasue the body of knowledge of genetic and epigenetic variants has many 
@@ -50,6 +70,7 @@ const About = (props) => {
             This app was designed and developed by the PGP-UK/GenoME team.
           </PageText>
         </ImageBackground>
+        </Block> 
         </Section>
       </PageLayout>
     </>
@@ -60,6 +81,7 @@ const styles = StyleSheet.create({
 
   headerRow: {
     marginBottom: 25,
+    
   },
 
   main_text: {
@@ -75,21 +97,25 @@ const styles = StyleSheet.create({
   bottom_text: {
     color: '#666E7A',
     fontSize: 10,
-    marginBottom: 5,
+    position: 'relative',
+    marginTop: 5,
+    
   },
 
   bottom_red_text: {
     color: '#ff0000',
     fontSize: 10,
     marginBottom: 5,
-    textAlign: "justify",
+    textAlign: 'auto'
+    //position: 'relative',
   },
 
   bottomImage:{
     height: 225,
-    width: 380,
+    width: 370,
     marginTop: 20,
-    justifyContent:"flex-end",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
   socialMedia:{

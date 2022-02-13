f8b50fc263281819f781b420c1943a0217235c12
Nathaniel Donato <nathanieldonatofx@gmail.com>
2022-02-13 12:57:31 +0200

    Smoking page layout created

--- screens/individual/Smoking/CenterImage.js
+++ screens/individual/Smoking/CenterImage.js
@@ -4,38 +4,36 @@ import { Section, Block } from 'react-native-responsive-layout';
 import { withSizeInfo } from 'react-native-responsive-layout/wrappers';
 import { useSafeAreaFrame } from 'react-native-safe-area-context';
 
-const CenterImage = withSizeInfo(({ sizeSelector, data, ...props }) => {
-  const ImageStyles = sizeSelector({ xs: styles.imageSm, md: styles.imageMd });
-  const imageWidth = Math.round(useSafeAreaFrame().width * 0.8) - 40;
-  const maxImageHeight = Math.round(useSafeAreaFrame().height * 0.5);
+const CenterImage = withSizeInfo(({ sizeSelector, ...props }) => {
+  const imagePercentage = sizeSelector({
+    xs: 0.8,
+    md: 0.8,
+    xl: 0.65,
+    xxl: 0.4,
+  });
+  const imageWidth =
+    Math.round(useSafeAreaFrame().width * imagePercentage) - 40;
+  const maxImageHeight = Math.round(useSafeAreaFrame().height * 0.8);
   const finalImageWidth =
     imageWidth > maxImageHeight ? maxImageHeight : imageWidth;
 
   return (
-    <Section>
-      <Block>
-        <View
-          style={{
-            justifyContent: 'center',
-            flex: 1,
-          }}>
-          <Image
-            {...props}
-            style={[ImageStyles, { width: finalImageWidth }]}
-            resizeMode="contain"
-          />
-        </View>
-      </Block>
-    </Section>
+    <View style={{ justifyContent: 'center', flex: 1 }}>
+      <Image
+        {...props}
+        style={[
+          styles.image,
+          { width: finalImageWidth, height: finalImageWidth },
+        ]}
+        resizeMode="contain"
+      />
+    </View>
   );
 });
 
 const styles = StyleSheet.create({
-  imageMd: {
-    alignSelf: 'center',
+  image: {
     marginTop: 20,
-  },
-  imageSm: {
     alignSelf: 'center',
   },
 });
--- screens/individual/Smoking/index.js
+++ screens/individual/Smoking/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PageLayout from '../../../components/PageLayout';
+import { PageHeader } from '../../../components/Text';
 import BottomSection from './BottomSection';
 import CenterImage from './CenterImage';
 import SmokingExText from './SmokingExText';
+import ImageKey from './Imagekey.js';
 import { StyleSheet, Text } from 'react-native';
+import { Section, Block } from 'react-native-responsive-layout';
 
 const dataFile = {
   data: require(`../../../assets/data/age/data.json`),
@@ -31,12 +34,99 @@ const Smoking = (props) => {
 
   return (
     <PageLayout>
-      <Text style={[styles.heading]}>Smoking Prediction</Text>
-      <SmokingExText data={dataFile.data[name]} />
-      <CenterImage
-        source={AmbassadorsData[name].image}
-        data={dataFile.data[name]}
-      />
+      <PageHeader style={[styles.heading]}>Smoking Prediction</PageHeader>
+      <Block
+        xsSize="100%"
+        smSize="100%"
+        mdSize="100%"
+        lgSize="100%"
+        xlSize="100%"
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignContent: 'center',
+          paddingBottom: 20,
+        }}
+        xsHidden
+        smHidden
+        mdVisible
+        xxlHidden>
+        <SmokingExText data={dataFile.data[name]} />
+      </Block>
+      <Section
+        stretch
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignContent: 'center',
+          flexWrap: 'wrap',
+        }}>
+        <Block
+          xsSize="100%"
+          smSize="100%"
+          mdSize="50%"
+          lgSize="50%"
+          xlSize="50%"
+          style={{ paddingBottom: 20 }}>
+          <CenterImage
+            source={AmbassadorsData[name].image}
+            data={dataFile.data[name]}
+          />
+        </Block>
+        <Block
+          xsSize="0%"
+          smSize="0%"
+          mdSize="50%"
+          lgSize="50%"
+          xlSize="50%"
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            alignContent: 'center',
+            paddingBottom: 20,
+          }}
+          xsHidden
+          smHidden
+          mdHidden
+          xxlVisible>
+          <SmokingExText data={dataFile.data[name]} />
+        </Block>
+      </Section>
+      <Block
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignContent: 'center',
+          paddingBottom: 20,
+        }}>
+        <ImageKey data={data} />
+      </Block>
+      <Block
+        xsSize="100%"
+        smSize="100%"
+        mdSize="50%"
+        lgSize="50%"
+        xlSize="50%"
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignContent: 'center',
+          paddingBottom: 20,
+        }}
+        xsVisible
+        smVisible
+        mdHidden
+        lgHidden
+        xlHidden
+        xxlHidden>
+        <SmokingExText data={dataFile.data[name]} />
+      </Block>
       <BottomSection data={dataFile.data[name]} />
     </PageLayout>
   );

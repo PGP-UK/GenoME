c97a02395b972e14437b0150db31f6c29119afc3
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2022-08-07 14:41:32 +0100

    Add expo slash screen to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Asset } from "expo-asset";
@@ -10,9 +10,13 @@ import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
 import AppNavigator from "./components/AppNavigator";
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 const App = () => {
   const [appReady, setAppReady] = useState(false);
 
+  /*
   if (!appReady) {
     return (
       <AppLoading
@@ -21,6 +25,42 @@ const App = () => {
         onError={console.warn}
       />
     );
+  } 
+  */
+
+  //code below from https://docs.expo.dev/versions/latest/sdk/splash-screen/
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await startAsync={cacheResourcesAsync}
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appReady]);
+
+  if (!appReady) {
+    return null;
   }
 
   return (

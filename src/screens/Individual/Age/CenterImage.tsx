fbf499ff0c41aac90a8aab5353bc07ea4aa80cdf
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2022-09-22 17:20:53 +0100

    Centred age animation with padding

--- src/screens/Individual/Age/CenterImage.tsx
+++ src/screens/Individual/Age/CenterImage.tsx
@@ -1,33 +1,46 @@
-import React, {useRef, useState} from "react";
-import { StyleSheet, View } from "react-native";
-import { Block } from "react-native-responsive-layout";
-import { withSizeInfo } from "react-native-responsive-layout/wrappers";
-import { useSafeAreaFrame } from "react-native-safe-area-context";
+import React, { useRef, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Block } from 'react-native-responsive-layout';
+import { withSizeInfo } from 'react-native-responsive-layout/wrappers';
+import { useSafeAreaFrame } from 'react-native-safe-area-context';
 import Lottie from 'lottie-react-native';
-import LottieReset from "../../../components/LottieReset";
+import LottieReset from '../../../components/LottieReset';
 
 const CenterImage = withSizeInfo(({ sizeSelector, ...props }) => {
   const lottieRef = useRef();
-  const [displayReset, setDisplayReset] = useState(false)
+  const [displayReset, setDisplayReset] = useState(false);
 
   const { image } = props;
   const imagePercentage = sizeSelector({ xs: 0.8, md: 0.8, xl: 0.8, xxl: 0.4 });
-  const imageWidth =
-    Math.round(useSafeAreaFrame().width * imagePercentage);
+  const imageWidth = Math.round(useSafeAreaFrame().width * imagePercentage);
   const maxImageHeight = Math.round(useSafeAreaFrame().height * 0.8);
   const finalImageWidth =
     imageWidth > maxImageHeight ? maxImageHeight : imageWidth;
 
   return (
     <>
-      <LottieReset lottieRef={lottieRef} displayReset={displayReset}/>
-      <View style={{ alignContent: "center"}}>
-        <Lottie ref={lottieRef} source={image} autoPlay loop={false} progress={1}
+      <LottieReset lottieRef={lottieRef} displayReset={displayReset} />
+      <View
+        style={{
+          alignContent: 'center',
+        }}>
+        <Lottie
+          ref={lottieRef}
+          source={image}
+          autoPlay
+          loop={false}
+          progress={1}
           style={[
             styles.image,
-            { width: finalImageWidth, height: finalImageWidth, alignSelf: "center" },
+            {
+              width: finalImageWidth,
+              height: finalImageWidth,
+              paddingLeft: 80,
+            },
           ]}
-          onAnimationFinish={() => {setDisplayReset(true)}}
+          onAnimationFinish={() => {
+            setDisplayReset(true);
+          }}
         />
       </View>
     </>
@@ -42,7 +55,7 @@ const CenterRow = ({ image }) => (
 
 const styles = StyleSheet.create({
   image: {
-    alignSelf: "center",
+    alignSelf: 'center',
   },
 });
 

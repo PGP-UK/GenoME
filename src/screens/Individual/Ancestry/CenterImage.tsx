46bde1992b28b1bbe7b2ca7bb67bb2c703ec3a85
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2022-09-25 14:37:58 +0100

    Change text to be consistent with other pages

--- src/screens/Individual/Ancestry/CenterImage.tsx
+++ src/screens/Individual/Ancestry/CenterImage.tsx
@@ -1,29 +1,39 @@
-import React, {useRef, useState} from "react";
-import { View } from "react-native";
-import { Section, Block } from "react-native-responsive-layout";
-import { withSizeInfo } from "react-native-responsive-layout/wrappers";
-import { useSafeAreaFrame } from "react-native-safe-area-context";
+import React, { useRef, useState } from 'react';
+import { View } from 'react-native';
+import { Section, Block } from 'react-native-responsive-layout';
+import { withSizeInfo } from 'react-native-responsive-layout/wrappers';
+import { useSafeAreaFrame } from 'react-native-safe-area-context';
 import Lottie from 'lottie-react-native';
-import LottieReset from "../../../components/LottieReset";
+import LottieReset from '../../../components/LottieReset';
 
 const Iconimage = withSizeInfo(({ sizeSelector, ...props }) => {
   const lottieRef = useRef();
-  const [displayReset, setDisplayReset] = useState(false)
+  const [displayReset, setDisplayReset] = useState(false);
   const { image } = props;
   const imagePercentage = sizeSelector({ xs: 1, xxl: 0.4 });
-  const imageWidth =
-    Math.round(useSafeAreaFrame().width * imagePercentage);
+  const imageWidth = Math.round(useSafeAreaFrame().width * imagePercentage);
   const maxImageHeight = Math.round(useSafeAreaFrame().height * 0.8);
   const finalImageWidth =
     imageWidth > maxImageHeight ? maxImageHeight : imageWidth;
 
   return (
     <>
-      <LottieReset lottieRef={lottieRef} displayReset={displayReset}/>
-      <View style={{ alignContent: "center"}}>
-        <Lottie ref={lottieRef} source={image} autoPlay loop={false} progress={1}
-          style={{ width: finalImageWidth, height: finalImageWidth, alignSelf: "center" }}
-          onAnimationFinish={() => {setDisplayReset(true)}}
+      <LottieReset lottieRef={lottieRef} displayReset={displayReset} />
+      <View style={{ alignContent: 'center', paddingLeft: 80 }}>
+        <Lottie
+          ref={lottieRef}
+          source={image}
+          autoPlay
+          loop={false}
+          progress={1}
+          style={{
+            width: finalImageWidth,
+            height: finalImageWidth,
+            alignSelf: 'center',
+          }}
+          onAnimationFinish={() => {
+            setDisplayReset(true);
+          }}
         />
       </View>
     </>

42c0735524aed4fc4a36d76076c02864f19ac20e
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2022-09-25 14:37:58 +0100

    Change text to be consistent with other pages

--- src/screens/Individual/Ancestry/TopText.tsx
+++ src/screens/Individual/Ancestry/TopText.tsx
@@ -1,17 +1,13 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import { Text } from "@ui-kitten/components";
-import { Section, Block } from "react-native-responsive-layout";
-import { withSizeInfo } from "react-native-responsive-layout/wrappers";
-import CenterImage from "./CenterImage";
-import { PageHeader } from "../../../components/Text";
-import { PageText } from "../../../components/Text";
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Text } from '@ui-kitten/components';
+import { Section, Block } from 'react-native-responsive-layout';
+import { withSizeInfo } from 'react-native-responsive-layout/wrappers';
+import CenterImage from './CenterImage';
+import { PageHeader } from '../../../components/Text';
+import { PageText } from '../../../components/Text';
 
 const TopText = withSizeInfo(({ sizeSelector, data, image }) => {
-  const headerStyles = sizeSelector({
-    xs: styles.headerxs,
-    lg: styles.headerlg,
-  });
   const percent_boxStyles = sizeSelector({
     xs: styles.percent_boxxs,
     md: styles.percent_boxmd,
@@ -21,7 +17,7 @@ const TopText = withSizeInfo(({ sizeSelector, data, image }) => {
   return (
     <Section>
       <Block xsSize="100%" mdSize="35%">
-        <PageHeader style={headerStyles}>Ancestry</PageHeader>
+        <PageHeader style={styles.header}>Ancestry</PageHeader>
         <PageText category="p1" style={styles.main_text}>
           Different populations have their own distinctive pattern of genetic
           variants. By comparing thousands of my variants with those of
@@ -38,19 +34,17 @@ const TopText = withSizeInfo(({ sizeSelector, data, image }) => {
           style={{
             fontSize: 45,
             color: data.themeColor,
-            textAlign: "right",
-          }}
-        >
+            textAlign: 'right',
+          }}>
           {data.most_percent}
         </Text>
         <Text
           style={{
             fontSize: 45,
             color: data.themeColor,
-            textAlign: "right",
+            textAlign: 'right',
             marginTop: 15,
-          }}
-        >
+          }}>
           {data.other_percent_1}
         </Text>
       </Block>
@@ -59,40 +53,26 @@ const TopText = withSizeInfo(({ sizeSelector, data, image }) => {
 });
 
 const styles = StyleSheet.create({
-  headerxs: {
-    color: "#666E7A",
-    marginTop: 60,
-    marginBottom: 20,
-    marginLeft: 30,
-  },
-  headerlg: {
-    color: "#666E7A",
-    marginTop: 50,
-    marginBottom: 20,
-    marginLeft: 20,
+  header: {
+    color: '#63BEE1',
+    marginBottom: 25,
   },
   main_text: {
-    // color: '#666E7A',
-    // fontSize: 25,
     marginBottom: 20,
-    marginLeft: 30,
-    marginRight: 50,
-    lineHeight: 45,
-    textAlign: "justify",
   },
   percent_boxxs: {
-    alignItems: "center",
+    alignItems: 'center',
     marginBottom: 20,
     marginTop: 50,
   },
   percent_boxmd: {
-    alignItems: "flex-end",
+    alignItems: 'flex-end',
     marginBottom: 20,
     marginTop: 100,
     paddingRight: 50,
   },
   percent_boxlg: {
-    alignItems: "flex-end",
+    alignItems: 'flex-end',
     marginBottom: 20,
     marginTop: 100,
     paddingRight: 50,
@@ -102,15 +82,15 @@ const styles = StyleSheet.create({
   },
   rightTextxs: {
     fontSize: 35,
-    textAlign: "center",
+    textAlign: 'center',
   },
   rightTextmd: {
     fontSize: 35,
-    textAlign: "center",
+    textAlign: 'center',
   },
   rightTextlg: {
     fontSize: 35,
-    textAlign: "right",
+    textAlign: 'right',
   },
   // // rightTextlowerxs: {
   // //   fontSize: 45,

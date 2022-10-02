47308f3cb590a68caac8c6c10e9fc4e21fb1fa88
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2022-10-02 14:48:05 +0100

    121 Fix text image overlap on sm screens

--- src/screens/About/SocialMedia.tsx
+++ src/screens/About/SocialMedia.tsx
@@ -1,72 +1,55 @@
-import React from "react";
-import { StyleSheet, View, TouchableWithoutFeedback } from "react-native";
-import { Text } from "@ui-kitten/components";
-import * as Linking from "expo-linking";
-import FastImage from "react-native-fast-image";
+import React from 'react';
+import { StyleSheet, View, TouchableWithoutFeedback } from 'react-native';
+import { Text } from '@ui-kitten/components';
+import * as Linking from 'expo-linking';
+import FastImage from 'react-native-fast-image';
 
 const SocialMedia = () => (
-  <View style={[styles.container, { flexDirection: "column" }]}>
+  <View style={[styles.container, { flexDirection: 'column' }]}>
     <View
-      style={{ flex: 1, flexDirection: "row", justifyContent: "space-evenly" }}
-    >
+      style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-evenly' }}>
       <Text
         style={styles.socialMediaText}
-        onPress={() => Linking.openURL("https://twitter.com/pgpuk_genomes")}
-      >
+        onPress={() => Linking.openURL('https://twitter.com/pgpuk_genomes')}>
         Twitter
       </Text>
       <TouchableWithoutFeedback
-        onPress={() => Linking.openURL("https://twitter.com/pgpuk_genomes")}
-      >
+        onPress={() => Linking.openURL('https://twitter.com/pgpuk_genomes')}>
         <FastImage
           style={styles.socialMediaLogo}
           source={{
-            uri: "https://cdn.jsdelivr.net/gh/PGP-UK/GenoME/assets/images/about_us/more-information_twitter.png",
+            uri: 'https://cdn.jsdelivr.net/gh/PGP-UK/GenoME/assets/images/about_us/more-information_twitter.png',
           }}
         />
       </TouchableWithoutFeedback>
       <Text
         style={styles.socialMediaText}
-        onPress={() => Linking.openURL("https://pgpukblog.wordpress.com/")}
-      >
+        onPress={() => Linking.openURL('https://pgpukblog.wordpress.com/')}>
         Blogs
       </Text>
       <TouchableWithoutFeedback
-        onPress={() => Linking.openURL("https://pgpukblog.wordpress.com/")}
-      >
+        onPress={() => Linking.openURL('https://pgpukblog.wordpress.com/')}>
         <FastImage
           style={styles.socialMediaLogo}
           source={{
-            uri: "https://cdn.jsdelivr.net/gh/PGP-UK/GenoME/assets/images/about_us/more-information_blogs.png",
+            uri: 'https://cdn.jsdelivr.net/gh/PGP-UK/GenoME/assets/images/about_us/more-information_blogs.png',
           }}
         />
       </TouchableWithoutFeedback>
       <Text
         style={styles.socialMediaText}
-        onPress={() => Linking.openURL("https://www.personalgenomes.org.uk/")}
-      >
+        onPress={() => Linking.openURL('https://www.personalgenomes.org.uk/')}>
         Web
       </Text>
       <TouchableWithoutFeedback
-        onPress={() => Linking.openURL("https://www.personalgenomes.org.uk/")}
-      >
+        onPress={() => Linking.openURL('https://www.personalgenomes.org.uk/')}>
         <FastImage
           style={styles.socialMediaLogo}
           source={{
-            uri: "https://cdn.jsdelivr.net/gh/PGP-UK/GenoME/assets/images/about_us/more-information_www.png",
+            uri: 'https://cdn.jsdelivr.net/gh/PGP-UK/GenoME/assets/images/about_us/more-information_www.png',
           }}
         />
       </TouchableWithoutFeedback>
-    </View>
-
-    <View style={{ flex: 1 }}>
-      <Text style={styles.main_text1}>
-        We look forward to making great science with you!
-      </Text>
-      <Text style={styles.main_text2}>
-        Director: Professor Stephan Beck {"\n"}
-        Location: University College London
-      </Text>
     </View>
   </View>
 );
@@ -74,29 +57,29 @@ const SocialMedia = () => (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-evenly",
+    justifyContent: 'space-evenly',
   },
 
   main_text2: {
-    color: "#666E7A",
+    color: '#666E7A',
     fontSize: 20,
-    textAlign: "left",
+    textAlign: 'left',
   },
   main_text1: {
-    color: "#666E7A",
+    color: '#666E7A',
     fontSize: 20,
     marginBottom: 10,
     paddingTop: 40,
     paddingBottom: 20,
-    textAlign: "left",
+    textAlign: 'left',
   },
   bottom_text: {
-    color: "#666E7A",
+    color: '#666E7A',
     fontSize: 10,
     marginBottom: 20,
   },
   bottom_red_text: {
-    color: "#ff0000",
+    color: '#ff0000',
     fontSize: 10,
     marginBottom: 20,
   },
@@ -105,7 +88,7 @@ const styles = StyleSheet.create({
     marginBottom: 25,
   },
   socialMediaText: {
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
     paddingVertical: 20,
   },
   socialMediaLogo: {

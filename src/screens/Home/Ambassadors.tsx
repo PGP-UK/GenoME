3e19626b87d7f9ff17c2dd7f8d035203b145a761
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2022-11-06 13:35:14 +0000

    Fix image layout on 2x2 images

--- src/screens/Home/Ambassadors.tsx
+++ src/screens/Home/Ambassadors.tsx
@@ -1,35 +1,35 @@
-import React from "react";
-import { StyleSheet, Pressable } from "react-native";
-import { PageText } from "../../components/Text";
-import { Section, Block } from "react-native-responsive-layout";
-import { withSizeInfo } from "react-native-responsive-layout/wrappers";
-import { useSafeAreaFrame } from "react-native-safe-area-context";
-import FastImage from "react-native-fast-image";
+import React from 'react';
+import { StyleSheet, Pressable } from 'react-native';
+import { PageText } from '../../components/Text';
+import { Section, Block } from 'react-native-responsive-layout';
+import { withSizeInfo } from 'react-native-responsive-layout/wrappers';
+import { useSafeAreaFrame } from 'react-native-safe-area-context';
+import FastImage from 'react-native-fast-image';
 
 const AllAmbassadors = [
   {
-    name: "Stephan",
-    hexId: "uk35C650",
-    image: "stephan.gif",
-    themeColor: "#8CD8C4",
+    name: 'Stephan',
+    hexId: 'uk35C650',
+    image: 'stephan.gif',
+    themeColor: '#8CD8C4',
   },
   {
-    name: "Laura",
-    hexId: "uk33D02F",
-    image: "laura.gif",
-    themeColor: "#F6BD4A",
+    name: 'Laura',
+    hexId: 'uk33D02F',
+    image: 'laura.gif',
+    themeColor: '#F6BD4A',
   },
   {
-    name: "Momodou",
-    hexId: "uk481F67",
-    image: "momodou.gif",
-    themeColor: "#D94553",
+    name: 'Momodou',
+    hexId: 'uk481F67',
+    image: 'momodou.gif',
+    themeColor: '#D94553',
   },
   {
-    name: "Colin",
-    hexId: "uk4CA868",
-    image: "colin.gif",
-    themeColor: "#9C82DE",
+    name: 'Colin',
+    hexId: 'uk4CA868',
+    image: 'colin.gif',
+    themeColor: '#9C82DE',
   },
 ];
 
@@ -65,20 +65,19 @@ const Ambassador = (props) => {
       mdSize="25%"
       lgSize="50%"
       xlSize="50%"
-      xxlSize="25%"
-    >
+      xxlSize="25%">
       <Pressable
         onPress={() =>
-          navigation.navigate("Landing", { name: name.toLowerCase() })
+          navigation.navigate('Landing', { name: name.toLowerCase() })
         }
-        style={styles.box}
-      >
+        style={styles.box}>
         <AmbassadorImage image={image} />
         <PageText category="p1" style={[styles.header, { color: themeColor }]}>
           {hexId}
         </PageText>
         <PageText category="p1" style={styles.secondLineText}>
-          Also known as: {name}
+          Also known as: {'\n'}
+          {name}
         </PageText>
       </Pressable>
     </Block>
@@ -90,13 +89,12 @@ const Ambassadors = ({ navigation }) => (
     stretch
     style={{
       flex: 1,
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      alignContent: "center",
-      flexWrap: "wrap",
-    }}
-  >
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      alignContent: 'center',
+      flexWrap: 'wrap',
+    }}>
     {AllAmbassadors.map((AmbassadorData, idx) => (
       <Ambassador key={idx} {...AmbassadorData} navigation={navigation} />
     ))}
@@ -105,19 +103,19 @@ const Ambassadors = ({ navigation }) => (
 
 const styles = StyleSheet.create({
   box: {
-    alignItems: "center",
+    alignItems: 'center',
     padding: 10,
     marginTop: 10,
     marginBottom: 10,
   },
   header: {
     fontSize: 24,
-    fontWeight: "500",
+    fontWeight: '500',
     marginTop: 10,
   },
   secondLineText: {
     fontSize: 20,
-    textAlign: "center",
+    textAlign: 'center',
   },
 });
 

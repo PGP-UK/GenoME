5cb4802a128b35ddd185880d5b818881e81e6c78
IsmailM <ismail.moghul@gmail.com>
2023-01-08 15:06:13 +0000

    update APP_VARIANT setup

--- app.config.js
+++ app.config.js
@@ -1,9 +1,9 @@
-const IS_PRODUCTION = ['production', 'preview'].includes(process.env.APP_VARIANT)
+const IS_DEVELOPMENT = ['development', 'simulator'].includes(process.env.APP_VARIANT)
 
 export default {
   expo: {
-    name: IS_PRODUCTION ? 'GenoME' : 'GenoME Client',
-    slug: IS_PRODUCTION ? 'PGP-UK-GenoME' : 'PGP-UK-GenoME-dev',
+    name: IS_DEVELOPMENT ? 'GenoME Client' : 'GenoME',
+    slug: IS_DEVELOPMENT ? 'PGP-UK-GenoME-dev' : 'PGP-UK-GenoME',
     version: '1.0.0',
     orientation: 'default',
     icon: './assets/icon.png',
@@ -19,27 +19,27 @@ export default {
     assetBundlePatterns: ['**/*'],
     ios: {
       supportsTablet: true,
-      bundleIdentifier: IS_PRODUCTION
-        ? 'uk.org.personalgenomes.genome'
-        : 'dev.uk.org.personalgenomes.genome',
+      bundleIdentifier: IS_DEVELOPMENT
+        ? 'dev.uk.org.personalgenomes.genome'
+        : 'uk.org.personalgenomes.genome'
     },
     android: {
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#FFFFFF',
       },
-      package: IS_PRODUCTION
-        ? 'uk.org.personalgenomes.genome'
-        : 'dev.uk.org.personalgenomes.genome',
+      package: IS_DEVELOPMENT
+        ? 'dev.uk.org.personalgenomes.genome'
+        : 'uk.org.personalgenomes.genome',
     },
     web: {
       favicon: './assets/favicon.png',
     },
     extra: {
       eas: {
-        projectId: IS_PRODUCTION
-          ? 'efd04051-a37b-40e5-b844-a7d1ddd87176'
-          : '2d0a3a0e-accc-4e0c-85cd-44d7e6cb0aa3',
+        projectId: IS_DEVELOPMENT
+          ? '2d0a3a0e-accc-4e0c-85cd-44d7e6cb0aa3'
+          : 'efd04051-a37b-40e5-b844-a7d1ddd87176',
       },
     },
   },

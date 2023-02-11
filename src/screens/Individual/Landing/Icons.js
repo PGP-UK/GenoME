f4e1051e3d02d81c95b8caa9e327a2661a70b68d
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2023-02-11 14:24:50 +0000

    Add traits button on landing page

--- src/screens/Individual/Landing/Icons.js
+++ src/screens/Individual/Landing/Icons.js
@@ -2,31 +2,36 @@ export default [
   {
     pageName: 'Variant',
     icon: 'fa-light fa-dna',
-    transform: 'shrink-7'
+    transform: 'shrink-7',
   },
   {
     pageName: 'Ancestry',
-    icon: "fa-light fa-earth-americas",
-    transform: 'shrink-5'
+    icon: 'fa-light fa-earth-americas',
+    transform: 'shrink-5',
   },
   {
     pageName: 'Eyes',
-    icon: "fa-light fa-eye",
-    transform: 'shrink-5'
+    icon: 'fa-light fa-eye',
+    transform: 'shrink-5',
   },
   {
     pageName: 'Health',
-    icon: "fa-light fa-file-waveform",
-    transform: 'shrink-6'
+    icon: 'fa-light fa-file-waveform',
+    transform: 'shrink-6',
   },
   {
     pageName: 'Age',
-    icon: "fa-light fa-cake-candles",
-    transform: 'shrink-6'
+    icon: 'fa-light fa-cake-candles',
+    transform: 'shrink-6',
   },
   {
     pageName: 'Smoking',
-    icon: "fa-light fa-smoking",
-    transform: 'shrink-6'
+    icon: 'fa-light fa-smoking',
+    transform: 'shrink-6',
+  },
+  {
+    pageName: 'Traits',
+    icon: 'fa-light fa-smoking',
+    transform: 'shrink-6',
   },
 ];

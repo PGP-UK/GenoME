25e7de1be3733892f5a50559047456dda7b6088d
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2023-03-26 16:32:50 +0100

    Add react skia pie chart animation

--- src/screens/Individual/Health/Diseases/MiddleSection.jsx
+++ src/screens/Individual/Health/Diseases/MiddleSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Block } from 'react-native-responsive-layout';
 import { withSizeInfo } from 'react-native-responsive-layout/wrappers';
 import { StyleSheet, View } from 'react-native';
@@ -7,6 +7,123 @@ import { PageText, PgpText } from '../../../../components/Text';
 import { PageHeader } from '../../../../components/Text';
 import LottieAnimation from '../../../../components/LottieAnimation';
 
+import SkiaChart, { SVGRenderer } from '@wuba/react-native-echarts/skiaChart';
+import { LegendComponent } from 'echarts/components';
+import { PieChart } from 'echarts/charts';
+import * as echarts from 'echarts/core';
+import { BarChart } from 'echarts/charts';
+import { TitleComponent, TooltipComponent, GridComponent} from 'echarts/components';
+
+// register extensions
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  SVGRenderer,
+  BarChart,
+  PieChart,
+  LegendComponent
+])
+
+const VariantRisk = withSizeInfo(({ sizeSelector, riskData }) => {
+  const ImageStyles = sizeSelector({
+    xs: styles.xsHeader3,
+    md: styles.mdHeader3,
+  });
+
+  return (
+    <>
+      <Block style={styles.container}>
+        <PageHeader category="h5" style={styles.header2}>
+          Risk with each variant type:
+        </PageHeader>
+      </Block>
+
+      <Block style={styles.container}>
+        <View style={styles.squareWrapper}>
+          <View style={styles.square1} />
+          <PageHeader category="h6" style={ImageStyles}>
+            {riskData.first}
+          </PageHeader>
+        </View>
+        <View style={styles.squareWrapper}>
+          <View style={styles.square2} />
+          <PageHeader category="h6" style={ImageStyles}>
+            {riskData.second}
+          </PageHeader>
+        </View>
+        <View style={styles.squareWrapper}>
+          <View style={styles.square3} />
+          <PageHeader category="h6" style={ImageStyles}>
+            {riskData.third}
+          </PageHeader>
+        </View>
+      </Block>
+    </>
+  );
+});
+
+const E_HEIGHT = 400;
+const E_WIDTH = 400;
+
+function SkiaComponent({ option }) {
+  const skiaRef = useRef(null);
+
+  useEffect(() => {
+    let chart;
+    if (skiaRef.current) {
+      chart = echarts.init(skiaRef.current, 'light', {
+        renderer: 'svg',
+        width: E_WIDTH,
+        height: E_HEIGHT,
+      });
+      chart.setOption(option);
+    }
+    return () => chart?.dispose();
+  }, [option]);
+
+  return <SkiaChart ref={skiaRef} />;
+}
+
+const option = {
+  tooltip: {
+    trigger: 'item',
+  },
+  legend: {
+    bottom: '2%',
+    left: 'center',
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: ['65%', '80%'],
+      avoidLabelOverlap: false,
+      label: {
+        show: false,
+        position: 'center',
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: 40,
+          fontWeight: 'bold',
+        },
+      },
+      labelLine: {
+        show: false,
+      },
+      data: [
+        { value: 3, name: 'Normal' },
+        { value: 5, name: 'Increased (+)' },
+        { value: 3, name: 'Increased (++)' },
+      ],
+      color: ['#977ABD', '#34BC99', '#F6BB41'],
+    },
+  ],
+};
+
+// To add the new animation <SkiaComponent option={option} />
 const MiddleSection = (props) => {
   const { risk, message, image, population: { colour, percent } } = props;
   return (
@@ -51,44 +168,6 @@ const MiddleSection = (props) => {
     </>
   );
 };
-
-const VariantRisk = withSizeInfo(({ sizeSelector, riskData }) => {
-  const ImageStyles = sizeSelector({
-    xs: styles.xsHeader3,
-    md: styles.mdHeader3,
-  });
-
-  return (
-    <>
-      <Block style={styles.container}>
-        <PageHeader category="h5" style={styles.header2}>
-          Risk with each variant type:
-        </PageHeader>
-      </Block>
-
-      <Block style={styles.container}>
-        <View style={styles.squareWrapper}>
-          <View style={styles.square1} />
-          <PageHeader category="h6" style={ImageStyles}>
-            {riskData.first}
-          </PageHeader>
-        </View>
-        <View style={styles.squareWrapper}>
-          <View style={styles.square2} />
-          <PageHeader category="h6" style={ImageStyles}>
-            {riskData.second}
-          </PageHeader>
-        </View>
-        <View style={styles.squareWrapper}>
-          <View style={styles.square3} />
-          <PageHeader category="h6" style={ImageStyles}>
-            {riskData.third}
-          </PageHeader>
-        </View>
-      </Block>
-    </>
-  );
-});
 
 const styles = StyleSheet.create({
   container: {
